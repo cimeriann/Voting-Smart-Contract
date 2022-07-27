78753b766fb3ee9336ede01b5cf7cdfe59e0b876
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-07-27 09:42:13 +0100

    Change comments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,8 +16,7 @@ async function main() {
   fs.writeFileSync("./app/__config.json", JSON.stringify(config, null, 2));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main()
   .then(() => process.exit(0))
   .catch(error => {
